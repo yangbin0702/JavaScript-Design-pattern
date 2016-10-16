1e7629f7baf35c562ce7fe0835e485e8586337c9
yangbib0702 <yangbin5914@sina.com>
2016-10-16 18:23:25 +0800

    observe

---
+++ Observer/Observer.js
@@ -0,0 +1,53 @@
+/**
+ * Created by Administrator on 2016/10/16.
+ */
+function ObserverList(){
+    this.observerList = [];
+}
+
+ObserverList.prototype.Add = function(obj){
+   return this.observerList.push(obj);
+};
+
+ObserverList.prototype.Empty = function(){
+    this.observerList = [];
+};
+
+ObserverList.prototype.Get = function(index){
+    if(index > -1 && index< this.obsererList.length){
+        return this.observerList[index];
+    }
+};
+
+ObserverList.prototype.Insert = function(obj,index){
+    var pointer = -1;
+    if(index ===0){
+        this.observerList.unshift(obj);
+        pointer = index;
+    }else if(index === this.observerList.length){
+        this.observerList.push(obj);
+        pointer = index;
+    }
+    return pointer;
+};
+
+ObserverList.prototype.IndexOf = function(obj,startIndex){
+    var i = startIndex,pointer = -1;
+    while(i < this.observerList.length){
+        if(this.observerList[i]===obj){
+            pointer = i;
+        }
+        i++;
+    }
+    return pointer;
+};
+
+ObserverList.prototype.RemoveIndexAt = function(index){
+    if(index === 0 ){
+        this.observerList.shift();
+    }else if(index === this.observerList.length-1){
+        this.observerList.pop();
+    }
+};
+
+
