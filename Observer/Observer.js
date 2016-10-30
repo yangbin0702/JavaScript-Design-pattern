0e7581db3f36789a9526d467564ad4f3e6e8306e
yangbib0702 <yangbin5914@sina.com>
2016-10-30 21:07:58 +0800

    observe

--- Observer/Observer.js
+++ Observer/Observer.js
@@ -1,6 +1,16 @@
 /**
  * Created by Administrator on 2016/10/16.
  */
+/*
+* Subject(目标)
+* 维护一系列的观察者，方便添加或删除观察者
+* Observer(观察者)
+* 为那些在目标状态发生改变时需获得通知的对象提供一个更新接口
+* ConcreteSubject(具体目标)
+* 状态发生改变时，向Observer发出通知，储存ConcreteObserver的状态
+* ConcreteObserver(具体观察者)
+* 存储一个指向ConcreteSubject的引用，实现Observer的更新接口，以使自身状态与目标的状态保持一致
+* */
 function ObserverList(){
     this.observerList = [];
 }
